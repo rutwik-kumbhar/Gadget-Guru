09308dbde25200960a92f9b2a0b0addb6e293714
Rutwik Kumbhar <kumbharrutwik@gmail.com>
2023-02-25 22:56:25 +0530

    day-5 task  done

--- Script/index.js
+++ Script/index.js
@@ -1,10 +1,29 @@
 let usersData = JSON.parse(localStorage.getItem('users')) || [];
+let logedUser = JSON.parse(localStorage.getItem('loged-user')) || {}
+
+
+let userInfo = document.querySelectorAll(".user-info");
+let navBtns = document.querySelectorAll(".nav-btns");
+
+if(logedUser.name){
+    userInfo.forEach((el)=>{
+        el.style.display = "block"
+    })
+    navBtns.forEach((el)=>{
+        el.style.display = "none"
+    })
+}else{
+    console.log("No")
+}
+
+
 
 getUserData()
 
 function getUserData(){
     let signin_form = document.querySelector("#sign-in");
     signin_form.addEventListener("submit",(e)=>{
+        e.preventDefault()
         let formData = {
             name : signin_form.name.value,
             email : signin_form.email.value,
@@ -12,11 +31,13 @@ function getUserData(){
             password : signin_form.password.value,
         }
         usersData.push(formData)
+        localStorage.setItem("users",JSON.stringify(usersData));
+        console.log(formData)
         let signin = document.querySelector("#signin-modal")
         signin.setAttribute("data-bs-dismiss",'modal')
         alert("Account Created");
-        localStorage.setItem("users",JSON.stringify(usersData));
     })
+    
 
 }
 
@@ -24,24 +45,34 @@ function getUserData(){
 function loginUser(){
     let login = false;
     let login_form = document.querySelector("#log-in")
+    let login_modal= document.querySelector("#login-modal")
+    login_modal.setAttribute("data-bs-dismiss",'modal')
     login_form.addEventListener('submit',(e)=>{
          e.preventDefault();
          let email = login_form.loginEmail.value;
          let password = login_form.loginPassword.value;
-
          for(let i=0;i<usersData.length;i++){
             if(usersData[i].email == email && usersData[i].password == password){
+                logedUser = usersData[i];
+                localStorage.setItem('loged-user',JSON.stringify(logedUser))
                 login = true
                 break;
             }
          }
          if(login){
             alert("Successs");
+            userInfo.forEach((el)=>{
+                el.style.display = "block"
+            })
+            navBtns.forEach((el)=>{
+                el.style.display = "none"
+            })
         }else{
             alert("wrong credential")
         }
+       
     })
-    
+
 }
 
 loginUser()
