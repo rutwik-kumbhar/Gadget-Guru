106b230969932e9726a2cb3b822d6968b87241fb
starboy1010 <we.pixelvixel@gmail.com>
2023-02-27 09:45:11 +0530

    testing

--- Script/cart.js
+++ Script/cart.js
@@ -90,10 +90,10 @@ function Display(cart) {
   }
 }
 // book btn
-// let bookBtn = document.getElementById("book");
-// bookBtn.addEventListener("click", function () {
-//   window.location.href = "./";
-// });
+let bookBtn = document.getElementById("book");
+bookBtn.addEventListener("click", function () {
+  location.href = "./pay.html";
+});
 //////////// nav-bar js part////////////
 // let usersData = JSON.parse(localStorage.getItem("users")) || [];
 // let logedUser = JSON.parse(localStorage.getItem("loged-user")) || {};
@@ -185,6 +185,7 @@ loginUser();
 let userInfoBtn = document.querySelector("#dropdownMenuUser");
 let userDiv = document.querySelector("#user-div");
 let btnClose = document.querySelector("#btn-close");
+// let buynow = document.querySelector("#book")
 
 userInfoBtn.addEventListener("click", () => {
   userDiv.style.display = "block";
@@ -193,3 +194,10 @@ userInfoBtn.addEventListener("click", () => {
 btnClose.addEventListener("click", () => {
   userDiv.style.display = "none";
 });
+
+// buynow.addEventListener("click", () => {
+//   window.location="../HTML/pay.html"
+// });
+// document.getElementById("book").onclick = function () {
+//   location.href = "../HTML/pay.html";
+// };
